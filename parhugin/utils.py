494b0e5355e632f7839ffb51cc8c1ead5d20a34b
kasra-hosseini <khosseini@turing.ac.uk>
2021-01-18 12:31:35 +0000

    cprint to have colored log functionality

--- parhugin/utils.py
+++ parhugin/utils.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+from datetime import datetime
 import multiprocessing as mp
+import socket
 import traceback
 
 class Process(mp.Process):
@@ -40,3 +42,53 @@ class Process(mp.Process):
         if self._pconn.poll():
             self._exception = self._pconn.recv()
         return self._exception
+
+
+# ------------------- bc --------------------
+class bc:
+    lgrey = '\033[1;90m'
+    grey = '\033[90m'           # broing information
+    yellow = '\033[93m'         # FYI
+    orange = '\033[0;33m'       # Warning
+
+    lred = '\033[1;31m'         # there is smoke
+    red = '\033[91m'            # fire!
+    dred = '\033[2;31m'         # Everything is on fire
+
+    lblue = '\033[1;34m'
+    blue = '\033[94m'
+    dblue = '\033[2;34m'
+
+    lgreen = '\033[1;32m'       # all is normal
+    green = '\033[92m'          # something else
+    dgreen = '\033[2;32m'       # even more interesting
+
+    lmagenta = '\033[1;35m'
+    magenta = '\033[95m'        # for title
+    dmagenta = '\033[2;35m'
+
+    cyan = '\033[96m'           # system time
+    white = '\033[97m'          # final time
+
+    black = '\033[0;30m'
+
+    end = '\033[0m'
+    bold = '\033[1m'
+    under = '\033[4m'
+
+# ------------------- get_time --------------------
+def get_time():
+    time = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+    return time
+
+# ------------------- cprint --------------------
+def cprint(type_info, bc_color, text):
+    """
+    simple print function used for colored logging
+    """
+    ho_nam = socket.gethostname().split('.')[0]
+
+    print(bc.green          + get_time() + bc.end,
+          bc.magenta        + ho_nam     + bc.end,
+          bc.bold + bc.grey + type_info  + bc.end,
+          bc_color          + text       + bc.end)
