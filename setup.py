5a8f74078320508baa13978381887247b46f9e7f
kasra-hosseini <khosseini@turing.ac.uk>
2021-05-11 22:14:52 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="parhugin",
-    version="0.0.1",
+    version="0.0.2",
     description="Parallelize Python codes",
     author=u"Kasra Hosseini",
     #author_email="",
